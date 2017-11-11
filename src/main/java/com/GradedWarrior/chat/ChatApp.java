7f9dd84694c181d9d847847aa6db1aa3d768149f
alexcouch <alcouch65@gmail.com>
2017-11-11 00:31:03 -0800

    Long delay, and messages display twice

--- src/main/java/com/GradedWarrior/chat/ChatApp.java
+++ src/main/java/com/GradedWarrior/chat/ChatApp.java
@@ -10,12 +10,17 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ChatApp.MODID, version = ChatApp.VERSION)
+@Mod(modid = ChatApp.MODID, version = ChatApp.VERSION, name = ChatApp.NAME)
 public class ChatApp
 {
     public static final String MODID = "chat";
     public static final String VERSION = "1.0";
+    public static final String NAME = "minechat-app";
+
+    public static final Logger LOGGER = LogManager.getLogger(MODID.toUpperCase());
     
     @EventHandler
     public void init(FMLInitializationEvent event)
--- src/main/java/com/GradedWarrior/chat/app/ChatRApplication.java
+++ src/main/java/com/GradedWarrior/chat/app/ChatRApplication.java
@@ -1,34 +1,13 @@
 package com.GradedWarrior.chat.app;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.jar.JarException;
-
+import com.GradedWarrior.chat.ChatApp;
 import com.mrcrayfish.device.api.app.Application;
-import com.mrcrayfish.device.api.app.Component;
-import com.mrcrayfish.device.api.app.Dialog;
-import com.mrcrayfish.device.api.app.Dialog.Input;
-import com.mrcrayfish.device.api.app.Dialog.Message;
-import com.mrcrayfish.device.api.app.Dialog.OpenFile;
-import com.mrcrayfish.device.api.app.Dialog.SaveFile;
-import com.mrcrayfish.device.api.app.Layout;
-import com.mrcrayfish.device.api.app.listener.ClickListener;
-import com.mrcrayfish.device.api.io.File;
-import com.mrcrayfish.device.api.utils.BankUtil;
-
 import com.mrcrayfish.device.api.utils.OnlineRequest;
 import com.mrcrayfish.device.api.utils.OnlineRequest.ResponseHandler;
+import com.mrcrayfish.device.api.app.component.*;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.NBTTagCompound;
-
-import java.net.*;
-import java.io.*;
-import com.google.gson.*;
-import com.mrcrayfish.device.api.app.component.*;
 
 /**
  * Author: GradedWarrior
@@ -45,37 +24,11 @@ public class ChatRApplication extends Application {
 	public String currentData;
 	boolean urlData;
 	private int counter;
-	// private static final String USERNAME = ChatBar.getText();
 
-	/*
-	* 
-	*
-	*
-	*
-	*
-	*
-	*
-	*
-	* Type here ^
-	* 
-	* Note: MC is in debug mode
-	*/
-	
-	
-	
 	@Override
 	public void init() {
 		this.setDefaultHeight(130);
-		chat = new ItemList<String>(5, 5, 190, 7);
-
-		OnlineRequest.getInstance().make("http://centralos.x10host.com/chat/chat.html", new ResponseHandler() {
-			@Override
-			public void handle(boolean success, String response) {
-
-				oldData = response;
-
-			}
-		});
+		chat = new ItemList<>(5, 5, 190, 7);
 
 		addComponent(chat);
 		ChatBar = new TextField(6, 112, 155);
@@ -83,64 +36,52 @@ public class ChatRApplication extends Application {
 		addComponent(ChatBar);
 
 		Send = new Button(165, 112, 30, 15, "Send");
+        Send.setClickListener((arg0, arg1)->
+                OnlineRequest.getInstance().make(
+                        "http://centralos.x10host.com/chat/message.php?message=" +
+                                getFormattedStringData(ChatBar.getText()),
+                        (success, response)-> {
+                            if(currentData != null) {
+                                ChatBar.setText("");
+                            }
+                        })
+
+        );
 		addComponent(Send);
 	}
 
-	@Override
+    @Override
 	public void onTick() {
-		counter++;
-	    if ((counter % 100) == 0) { //Every 5 seconds (tick / 100 -> 0 remainders)
+        super.onTick();
 
-	    OnlineRequest.getInstance().make("http://centralos.x10host.com/chat/chat.html", new ResponseHandler() {
-	        @Override
-	        public void handle(boolean success, String response) {
+        counter++;
+        if ((counter % 20) == 0) { //Every second (tick / 20 -> 0 remainders)
 
+            OnlineRequest.getInstance().make("http://centralos.x10host.com/chat/chat.html", new ResponseHandler() {
+                @Override
+                public void handle(boolean success, String response) {
 
-	            if(currentData != oldData){
-	            	oldData = currentData;
-	                currentData = response; 
-	                chat.addItem("§9" + Minecraft.getMinecraft().player.getName() + ": " + response);
-	            }
-	           
+                    if (currentData == null) {
+                        currentData = response;
+                    }
+                    if (!currentData.equals(oldData)) {
+                        oldData = currentData;
+                        currentData = response;
+                        chat.addItem("§9" + Minecraft.getMinecraft().player.getName() + ": " + currentData);
+                        ChatApp.LOGGER.info(currentData);
+                    } else {
+                        oldData = response;
+                    }
+                }
+            });
 
-	        }
-
-
-	    });
-
-	}
-
-		Timer timer = new Timer();
-		Timer timer2 = new Timer();
-		/*
-		 * @Override protected void handleTick() { timer++; if(timer % 60 == 0)
-		 * { //Do request } }
-		 * 
-		 */
-		// timer.schedule(new SayHello(), 0, 5000);
-		// timer2.schedule(new SayHello2(), 0, 5000);
-
-		Send.setClickListener(new ClickListener() {
-
-			public void onClick(Component arg0, int arg1) {
-
-				OnlineRequest.getInstance().make(
-						"http://centralos.x10host.com/chat/message.php?message=" + ChatBar.getText() + "",
-						new ResponseHandler() {
-
-							public void handle(boolean success, String response) {
-
-								ChatBar.setText("");
-								// new SayHello();
-
-							}
-						});
-
-			}
-
-		});
+        }
 
 	}
+
+	private String getFormattedStringData(String toFormat){
+	    return toFormat.replace(" ", "%20");
+    }
 
 	@Override
 	public void load(NBTTagCompound nbtTagCompound) {
